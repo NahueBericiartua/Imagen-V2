3e3a90b841a1b92bc645c3cb00cc53be0a9d4a7e
NahueBericiartua <nahue.br1@gmail.com>
2021-10-09 18:25:50 -0300

    se agrega el proyecto

---
+++ Imagen V2/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Nahue
+ * Date: 8/10/2021
+ * Time: 18:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Imagen_V2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button btn_generar;
+		private System.Windows.Forms.PictureBox pic_imagen;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btn_generar = new System.Windows.Forms.Button();
+			this.pic_imagen = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pic_imagen)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btn_generar
+			// 
+			this.btn_generar.Location = new System.Drawing.Point(100, 12);
+			this.btn_generar.Name = "btn_generar";
+			this.btn_generar.Size = new System.Drawing.Size(178, 60);
+			this.btn_generar.TabIndex = 0;
+			this.btn_generar.Text = "Generar";
+			this.btn_generar.UseVisualStyleBackColor = true;
+			this.btn_generar.Click += new System.EventHandler(this.Btn_generarClick);
+			// 
+			// pic_imagen
+			// 
+			this.pic_imagen.Location = new System.Drawing.Point(12, 78);
+			this.pic_imagen.Name = "pic_imagen";
+			this.pic_imagen.Size = new System.Drawing.Size(344, 250);
+			this.pic_imagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pic_imagen.TabIndex = 1;
+			this.pic_imagen.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(368, 340);
+			this.Controls.Add(this.pic_imagen);
+			this.Controls.Add(this.btn_generar);
+			this.Name = "MainForm";
+			this.Text = "Imagen V2";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pic_imagen)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Imagen V2/MainForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Nahue
+ * Date: 8/10/2021
+ * Time: 18:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Imagen_V2
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string[] direccion;
+		string dir;
+		Random n;
+		int i;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			n = new Random();
+			i = new int();
+			direccion = new string[5];
+			dir = "C:\\Users\\Nahue\\Documents\\SharpDevelop Projects\\Imagen V2\\Imagenes\\";
+			direccion[0]=dir+"1.png";
+			direccion[1]=dir+"2.png";
+			direccion[2]=dir+"3.png";
+			direccion[3]=dir+"4.png";
+			direccion[4]=dir+"5.png";
+		}
+		void Btn_generarClick(object sender, EventArgs e)
+		{
+			i=Convert.ToInt32(n.Next(0,5));
+			pic_imagen.Image = Image.FromFile(direccion[i]);
+		}
+	}
+}
